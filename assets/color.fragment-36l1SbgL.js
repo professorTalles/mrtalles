576ac7cecd03ba8f172d7a52d28c15b7290f0abf
professorTalles <45723759+professorTalles@users.noreply.github.com>
2026-08-26 23:52:32 -0300

    Add files via upload

---
+++ assets/color.fragment-36l1SbgL.js
@@ -0,0 +1,20 @@
+import{z as e}from"./index-Cy5d9OtD.js";import{c as a,f as i,a as d,b as l}from"./fogFragment-C494MdEf.js";const n="colorPixelShader",r=`#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+#define VERTEXCOLOR
+varying vec4 vColor;
+#else
+uniform vec4 color;
+#endif
+#include<clipPlaneFragmentDeclaration>
+#include<fogFragmentDeclaration>
+#define CUSTOM_FRAGMENT_DEFINITIONS
+void main(void) {
+#define CUSTOM_FRAGMENT_MAIN_BEGIN
+#include<clipPlaneFragment>
+#if defined(VERTEXCOLOR) || defined(INSTANCESCOLOR) && defined(INSTANCES)
+gl_FragColor=vColor;
+#else
+gl_FragColor=color;
+#endif
+#include<fogFragment>(color,gl_FragColor)
+#define CUSTOM_FRAGMENT_MAIN_END
+}`;e.ShadersStore[n]||(e.ShadersStore[n]=r);const c=[a,i,d,l];for(const o of c)e.IncludesShadersStore[o.name]||(e.IncludesShadersStore[o.name]=o.shader);const S={name:n,shader:r};export{S as colorPixelShader};
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sphere-world-v2";
+const CACHE_NAME = "sphere-world-v3";
 const BASE_PATH = new URL(".", self.registration.scope).pathname;
 const APP_SHELL = [BASE_PATH, `${BASE_PATH}index.html`, `${BASE_PATH}manifest.webmanifest`];
 
