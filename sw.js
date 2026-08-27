6dd045604820c4f44278d36964d1d4575b274ec3
professorTalles <45723759+professorTalles@users.noreply.github.com>
2026-08-27 12:16:58 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "sphere-world-v4-6x6";
+const CACHE_NAME = "sphere-world-v5-vertical";
 const BASE_PATH = new URL(".", self.registration.scope).pathname;
 const APP_SHELL = [BASE_PATH, `${BASE_PATH}index.html`, `${BASE_PATH}manifest.webmanifest`];
 
